b1b80186b5cd2b50ed8ae52ee18ff1ee77a3fffa
Terry Yun <nooheat1228@gmail.com>
2017-08-12 23:23:57 +0900

    [modified] /post CRUD routes, [use] authMiddleware

--- server/routes/api/index.js
+++ server/routes/api/index.js
@@ -1,7 +1,8 @@
 const router = require('express').Router();
-const auth = require('./auth')
-
+const auth = require('./auth');
+const posts = require('./posts');
+const authMiddleware = require('../../middlewares/auth');
 
 router.use('/auth', auth);
-
+router.use('/post', authMiddleware, posts);
 module.exports = router;
